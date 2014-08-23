ae2454024b8ff024e8410f8a02f249c3bf7405d1
Contra <contra@maricopa.edu>
2014-08-23 03:53:03 -0700

    play with my face

--- examples/glitch-client.js
+++ examples/glitch-client.js
@@ -2,13 +2,11 @@ var revisit = require('../');
 var fs = require('fs');
 var path = require('path');
 
-var data = fs.createReadStream(path.join(__dirname, 'yo.jpg'));
+var data = fs.createReadStream(path.join(__dirname, 'contra.jpg'));
 
-var glitch = revisit.transform('http://localhost:8080?intensity=10');
-var txt = revisit.transform('http://localhost:8081?text=yo');
+var glitch = revisit.transform('http://localhost:8080?intensity=1');
 
 // write file to server
 data
   .pipe(glitch())
-  .pipe(txt())
-  .pipe(fs.createWriteStream(path.join(__dirname, 'yo-glitched.jpg')));
+  .pipe(fs.createWriteStream(path.join(__dirname, 'contra-glitched.jpg')));
--- examples/glitch.js
+++ examples/glitch.js
@@ -12,14 +12,14 @@ var jpgRand = rng.bind(null, 1, 254);
 function glitch(opt) {
   // invert within 1
   // so 3 = 0.7, 5 = 0.5, 2 = 0.8, etc.
-  var intensity = parseInt(opt.intensity) || 1;
+  var intensity = (parseInt(opt.intensity) || 1)/10;
   return map(function(buf, idx){
     for (var i = 0; i < buf.length - 1; i++) {
       if (buf[i] !== jpgRand()) {
         continue;
       }
 
-      if ((Math.random()*10) >= intensity) {
+      if (Math.random() >= intensity) {
         continue;
       }
       buf[i] = jpgRand();
